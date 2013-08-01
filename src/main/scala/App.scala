689077cec3ce223c6f929d31e5b624df1aba4fdd
Chris Myers <christopher.myers@bskyb.com>
2013-08-01 21:21:22 +0100

    Blown away the answers with "TODO". hehehe

--- src/main/scala/App.scala
+++ src/main/scala/App.scala
@@ -23,11 +23,11 @@ object OptionalExercises {
 
         val config = Map[String,String]( "host" -> "sky.com", "port" -> "8080")
 
-	def host = config.get("host").getOrElse("Host not found in config")
+	def host = "TODO"
 
-	def lengthOfHost = config.get("host").map(_.length)
+	def lengthOfHost = "TODO"
 
-	def portPlus1000 = config.get("port").map(Integer.parseInt(_)).map(_+1000).map(_.toString)
+	def portPlus1000 = "TODO"
 }
 
 object OptionFlatMapExercises {
@@ -35,9 +35,9 @@ object OptionFlatMapExercises {
 	val hosts = Map("host1" -> "sky.com", "host2" -> "bskyb.com", "host3" -> "netflix.com")
 	val envs = Map("sky.com" -> "prod", "bskyb.com" -> "test")
 
-	def getEnvForHost(host:String) = hosts.get(host).flatMap(envs.get(_)).getOrElse("couldn't resolve")
+	def getEnvForHost(host:String) = "TODO"
 
-	def connectToSkyHostOnly(host:String) = hosts.get(host).flatMap{x:String => if (x.contains("sky")) Some(createConnection) else None}
+	def connectToSkyHostOnly(host:String) = "TODO"
 
 	def createConnection = "Connected"
 
