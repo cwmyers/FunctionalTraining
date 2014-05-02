d55fe7ccdd1f4690d000b7a94b650085b3f62caf
Chris Myers <chris.myers@rea-group.com>
2014-05-02 12:50:35 +1000

    Compose with changing types

--- src/main/scala/com/rea/higherorder/Composing.scala
+++ src/main/scala/com/rea/higherorder/Composing.scala
@@ -3,6 +3,7 @@ package com.rea.higherorder
 object Composing {
   def main(args: Array[String]) {
     println(addTwoMinusThree(6) + " should equal 5")
+    println(addTwoMinusThreePrettyPrint(2) + " should equal 'The number is 1'")
   }
 
   def add(a: Int, b: Int) = a + b
@@ -17,9 +18,13 @@ object Composing {
 
   // How do we create a new function from addTwo and minusThree?
   val addTwoMinusThree: Int => Int = ???
+  
+  def prettyPrint(i:Int) = s"The number is $i"
+  
+  val addTwoMinusThreePrettyPrint: Int => String = ???
 
   // How do we convert f and g into a brand new function?
-  def compose_[A, B, C](f: A => B, g: B => C): A => C = ???
+  def compose_[A, B, C](f: B => C, g: A => B): A => C = ???
 
 }
 
