920a39ef03a6ed50c47e2e380486e4bc546c2d55
Chris Myers <chris.myers@rea-group.com>
2014-05-02 14:58:06 +1000

    Box examples

---
+++ src/main/scala/com/rea/typesafety/Box.scala
@@ -0,0 +1,13 @@
+package com.rea.typesafety
+
+case class Box[A](get: A) {
+  def map[B](f: A => B): Box[B] = ???
+
+  def flatMap[B](f: A => Box[B]): Box[B] = ???
+}
+
+object BoxExercises extends App {
+  println("Box[6] + 3 = Box[9]: " + Box(6).map(_ + 3))
+  println("Box[4] + 2 = Box[6]: " + Box(4).flatMap(b => Box(b + 2)))
+}
+
