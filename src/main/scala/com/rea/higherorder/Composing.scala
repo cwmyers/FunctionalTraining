b2b7f5b2f625895db8835e6dea517b28aeeaf5e4
Chris Myers <chris.myers@rea-group.com>
2014-05-02 12:18:35 +1000

    Added answers for composing

--- src/main/scala/com/rea/higherorder/Composing.scala
+++ src/main/scala/com/rea/higherorder/Composing.scala
@@ -9,14 +9,14 @@ object Composing {
   def add(a: Int, b: Int) = a + b
 
   // We want to partially apply add
-  def addTwo: Int => Int = ???
+  def addTwo: Int => Int = add(2,_)
 
   def subtract(a: Int, b: Int) = a - b
 
   // We want to partially apply subtract
-  def minusThree: Int => Int = ???
+  def minusThree: Int => Int = subtract(_,3)
 
   // How do we create a new function from addTwo and minusThree?
-  val addTwoMinusThree: Int => Int = ???
+  val addTwoMinusThree: Int => Int = addTwo compose minusThree
 
 }
