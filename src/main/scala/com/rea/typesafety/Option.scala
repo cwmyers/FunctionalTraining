d32371ea42df67d08b7fea003ba64c3e3a650122
Chris Myers <chris.myers@rea-group.com>
2014-05-02 14:08:01 +1000

    More option Exercises

--- src/main/scala/com/rea/typesafety/Option.scala
+++ src/main/scala/com/rea/typesafety/Option.scala
@@ -1,10 +1,10 @@
 package com.rea.typesafety
 
-object App {
+object Options {
   def main(args: Array[String]) {
-    println("The host name is: " + OptionalExercises.host)
-    println("The length of host name is: " + OptionalExercises.lengthOfHost)
-    println("The port plus 1000 is: " + OptionalExercises.portPlus1000 + " (expecting 9080)")
+    println("The host name is: " + OptionalExercises.getFromConfig("host").getOrElse("Not Found"))
+    println("The length of host name is: " + OptionalExercises.lengthOfHost().getOrElse(0))
+    println("The port plus 1000 is: " + OptionalExercises.portPlus1000().getOrElse(0) + " (expecting 9080)")
 
     import OptionFlatMapExercises._
     println("Environment for host1 is " + getEnvForHost("host1") + " expecting 'prod'")
@@ -12,7 +12,7 @@ object App {
     println("Environment for host3 is " + getEnvForHost("host3") + " expecting 'couldn't resolve'")
     println("Environment for host4 is " + getEnvForHost("host4") + " expecting 'couldn't resolve'")
 
-    println("Should be connected to sky.com " + connectToSkyHostOnly("host1"))
+    println("Should be connected to rea.com " + connectToSkyHostOnly("host1"))
     println("Should be connected to bskyb.com " + connectToSkyHostOnly("host2"))
     println("Should not be connected to netflix.com " + connectToSkyHostOnly("host3"))
     println("Should not be connected to unknown host " + connectToSkyHostOnly("host4"))
@@ -21,24 +21,24 @@ object App {
 
 object OptionalExercises {
 
-  val config = Map[String, String]("host" -> "sky.com", "port" -> "8080")
+  val config = Map[String, String]("host" -> "rea.com", "port" -> "8080")
 
-  def host = "TODO"
+  def getFromConfig(key:String):Option[String] = ???
 
-  def lengthOfHost = "TODO"
+  def lengthOfHost():Option[Int] = ???
 
-  def portPlus1000 = "TODO"
+  def portPlus1000():Option[Int] = ???
 }
 
 object OptionFlatMapExercises {
 
-  val hosts = Map("host1" -> "sky.com", "host2" -> "bskyb.com", "host3" -> "netflix.com")
+  val hosts = Map("host1" -> "rea.com", "host2" -> "bskyb.com", "host3" -> "netflix.com")
   val envs = Map("sky.com" -> "prod", "bskyb.com" -> "test")
 
-  def getEnvForHost(host: String) = "TODO"
+  def getEnvForHost(host: String):Option[String] = ???
 
-  def connectToSkyHostOnly(host: String) = "TODO"
+  def connectToSkyHostOnly(host: String):String = ???
 
-  def createConnection = "Connected"
+  def createConnection(host:String):String = s"Connected to $host"
 
 }
