419840be3ffe3471cdb0628cb011b54403b0c4e1
Chris Myers <chris.myers@rea-group.com>
2014-05-02 15:41:20 +1000

    Box answers

--- src/main/scala/com/rea/typesafety/Box.scala
+++ src/main/scala/com/rea/typesafety/Box.scala
@@ -1,7 +1,12 @@
 package com.rea.typesafety
 
 case class Box[A](get: A) {
-  def map[B](f: A => B): Box[B] = Box(f(this.get))
+  def map[B](f: A => B): Box[B] = Box(f(get))
+
+  // Implementation with flatMap
+  def map2[B](f: A => B): Box[B] = flatMap(f andThen unit[B])
+
+  def unit[B](b: B) = Box(b)
 
   def flatMap[B](f: A => Box[B]): Box[B] = f(this.get)
 }
