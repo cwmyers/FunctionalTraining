f4f07ade09a124596611e20000103fca514eafd6
Chris Myers <chris.myers@rea-group.com>
2014-05-05 12:02:34 +1000

    Adding Recursion exercises

--- src/main/scala/com/rea/recursion/RecursionExercises.scala
+++ src/main/scala/com/rea/recursion/RecursionExercises.scala
@@ -27,12 +27,19 @@ object RecursionExercises {
   // Given a function from A => Boolean, return a list with only those item where the function returned true.
   def filter[A](x: List[A], f: A => Boolean): List[A] = ???
 
-  def main(args: List[String]) = {
+  def main(args: Array[String]) = {
     println("10 + 34 = 44: " + add(10, 34))
     println("0 + 34 = 34: " + add(0, 34))
     println("0 + 0 = 0: " + add(0, 0))
 
     println("Sum of List(1,2,3,4,5,6) = 21: " + sum(List(1, 2, 3, 4, 5, 6)))
     println("Length of List(1,2,3,4,5,6) = 6: " + length(List(1, 2, 3, 4, 5, 6)))
+
+    println("Add one to List(1,2,3,4,5,6) = List(2,3,4,5,6,7): " + map(List(1, 2, 3, 4, 5, 6), plusOne))
+
+    println("Remove elements under 4 for List(1,2,3,4,5,6) = List(4,5,6): " + filter(List(1, 2, 3, 4, 5, 6), {
+      x: Int => x < 4
+    }))
+
   }
 }
