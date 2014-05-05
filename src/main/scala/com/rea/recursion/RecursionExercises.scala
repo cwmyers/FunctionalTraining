4dbe00dcc57d996a7c19f894b2da17e8b07e53bf
Chris Myers <chris.myers@rea-group.com>
2014-05-05 12:10:52 +1000

    Adding Recursion exercises

--- src/main/scala/com/rea/recursion/RecursionExercises.scala
+++ src/main/scala/com/rea/recursion/RecursionExercises.scala
@@ -1,5 +1,7 @@
 package com.rea.recursion
 
+import scala.annotation.tailrec
+
 
 // Taken from http://tmorris.net/posts/scala-exercises-for-beginners/index.html
 object RecursionExercises {
@@ -9,10 +11,12 @@ object RecursionExercises {
   def minusOne(n: Int) = n - 1
 
   // Add two positive Integers together.  You are only allowed to use plusOne and minusOne above
+  @tailrec
   def add(a: Int, b: Int): Int = if (a == 0) b else add(minusOne(a), plusOne(b))
 
   // You are not permitted to use any list functions such as map, flatMap, ++, flatten etc
   def sum(l: List[Int]): Int = {
+    @tailrec
     def sum0(acc: Int, l1: List[Int]): Int = l1 match {
       case Nil => acc
       case (x: Int) :: xs => sum0(add(x, acc), xs)
@@ -23,6 +27,7 @@ object RecursionExercises {
 
   //Again no list functions are permitted for the following
   def length[A](x: List[A]): Int = {
+    @tailrec
     def length0(acc: Int, l: List[A]): Int = l match {
       case Nil => acc
       case h :: tail => length0(plusOne(acc), tail)
@@ -41,7 +46,16 @@ object RecursionExercises {
 
 
   // Given a function from A => Boolean, return a list with only those item where the function returned true.
-  def filter[A](x: List[A], f: A => Boolean): List[A] = ???
+  def filter[A](x: List[A], f: A => Boolean): List[A] = x match {
+    case Nil => Nil
+    case h :: t => {
+      val filter1: List[A] = filter(t, f)
+      if (f(h))
+        h :: filter1
+      else
+        filter1
+    }
+  }
 
   def main(args: Array[String]) = {
     println("10 + 34 = 44: " + add(10, 34))
@@ -54,7 +68,7 @@ object RecursionExercises {
     println("Add one to List(1,2,3,4,5,6) = List(2,3,4,5,6,7): " + map(List(1, 2, 3, 4, 5, 6), plusOne))
 
     println("Remove elements under 4 for List(1,2,3,4,5,6) = List(4,5,6): " + filter(List(1, 2, 3, 4, 5, 6), {
-      x: Int => x < 4
+      x: Int => x >= 4
     }))
 
   }
