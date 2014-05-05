34acf1191afaaef626f1d5ba52853be64b2e3e01
Chris Myers <chris.myers@rea-group.com>
2014-05-05 11:50:42 +1000

    Adding Recursion exercises

--- src/main/scala/com/rea/recursion/RecursionExercises.scala
+++ src/main/scala/com/rea/recursion/RecursionExercises.scala
@@ -12,17 +12,23 @@ object RecursionExercises {
   def add(a: Int, b: Int): Int = if (a == 0) b else add(minusOne(a), plusOne(b))
 
   // You are not permitted to use any list functions such as map, flatMap, ++, flatten etc
-  def sum(l: List[Int]): Int =  {
-    def sum0(acc:Int,l1: List[Int]):Int = l1 match {
+  def sum(l: List[Int]): Int = {
+    def sum0(acc: Int, l1: List[Int]): Int = l1 match {
       case Nil => acc
-      case (x:Int) :: xs => sum0(add(x,acc), xs)
+      case (x: Int) :: xs => sum0(add(x, acc), xs)
     }
 
-    sum0(0,l)
+    sum0(0, l)
   }
 
   //Again no list functions are permitted for the following
-  def length[A](x: List[A]): Int = ???
+  def length[A](x: List[A]): Int = {
+    def length0(acc: Int, l: List[A]): Int = l match {
+      case Nil => acc
+      case h :: tail => length0(plusOne(acc), tail)
+    }
+    length0(0, x)
+  }
 
   // Mapping over a list.  You are given a List of type A and a function converting an A to a B
   // and you give back a list of type B.  No list functions allowed!
@@ -35,5 +41,6 @@ object RecursionExercises {
     println("0 + 0 = 0: " + add(0, 0))
 
     println("Sum of List(1,2,3,4,5,6) = 21: " + sum(List(1, 2, 3, 4, 5, 6)))
+    println("Length of List(1,2,3,4,5,6) = 6: " + length(List(1, 2, 3, 4, 5, 6)))
   }
 }
