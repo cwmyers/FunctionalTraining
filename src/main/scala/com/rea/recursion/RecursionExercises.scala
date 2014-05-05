b286fb0982116b1757c3d495fdf996153154784e
Chris Myers <chris.myers@rea-group.com>
2014-05-05 10:57:49 +1000

    Adding Recursion exercises

--- src/main/scala/com/rea/recursion/RecursionExercises.scala
+++ src/main/scala/com/rea/recursion/RecursionExercises.scala
@@ -9,13 +9,20 @@ object RecursionExercises {
   def minusOne(n: Int) = n - 1
 
   // Add two Integers together.  You are only allowed to use plusOne and minusOne above
-  def add(a: Int, b: Int) = ???
+  def add(a: Int, b: Int): Int = if (a == 0) b else add(minusOne(a), plusOne(b))
 
   // You are not permitted to use any list functions such as map, flatMap, ++, flatten etc
-  def sum(l: List[Int]): Int = ???
+  def sum(l: List[Int]): Int =  {
+    def sum0(acc:Int,l1: List[Int]):Int = l1 match {
+      case Nil => acc
+      case (x:Int) :: xs => sum0(add(x,acc), xs)
+    }
+
+    sum0(0,l)
+  }
 
 
-  def main(args: List[String]) = {
+  def main(args: Array[String]) = {
     println("10 + 34 = 44: " + add(10, 34))
     println("0 + 34 = 34: " + add(0, 34))
     println("0 + 0 = 0: " + add(0, 0))
