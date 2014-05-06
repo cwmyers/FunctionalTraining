bbb57839172a6b603ef10eddc1b59ea18cbbc443
Chris Myers <chris.myers@rea-group.com>
2014-05-06 16:39:29 +1000

    Adding folding exercises

--- src/main/scala/com/rea/higherorder/FoldingExercises.scala
+++ src/main/scala/com/rea/higherorder/FoldingExercises.scala
@@ -9,10 +9,45 @@ object FoldingExercises {
    * and the next value in the list (of type A) and returns a value which will be feed back into
    * the accumulator of the next call.
    *
+   * As the name suggests it processes the list from left to right.
+   *
+   * Have a close look at your implementations from the RecursionExercises.  Which parts could you
+   * pull out to a function to make them all common?  Your implementation will be very close to
+   * foldLeft.
+   *
+   * Good luck!
+   *
    */
   def foldLeft[A, B](initialValue: B, list: List[A])(f: (B, A) => B): B = ???
 
+  /**
+   * foldRight is the same as foldLeft, except it processes the list from right to left.
+   */
   def foldRight[A,B](initialValue:B, list: List[A])(f: (A,B) => B):B = ???
+
+  /**
+   * Remember these, from our recursion exercises?  They can all be implemented with either
+   * foldLeft or foldRight.
+   */
+
+  def sum(l: List[Int]): Int = ???
+
+  def length[A](x: List[A]): Int = ???
+
+  def map[A, B](x: List[A], f: A => B): List[B] = ???
+
+  def filter[A](x: List[A], f: A => Boolean): List[A] = ???
+
+  def append[A](x: List[A], y: List[A]): List[A] = ???
+
+  def flatten[A](x: List[List[A]]): List[A] = ???
+
+  def flatMap[A, B](x: List[A], f: A => List[B]): List[B] = ???
+
+  // Maximum of the empty list is 0
+  def maximum(x: List[Int]): Int = ???
+
+  def reverse[A](x: List[A]): List[A] = ???
 
   def main(args: Array[String]) = {
     assert(foldLeft(0, List(1,2,3))(_+_) == 6)
