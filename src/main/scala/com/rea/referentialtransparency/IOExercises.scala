521e5503cdf04046d407acea9c4668e18247a4f0
Chris Myers <chris.myers@rea-group.com>
2014-05-20 16:53:37 +1000

    IO Exercises

--- src/main/scala/com/rea/referentialtransparency/IOExercises.scala
+++ src/main/scala/com/rea/referentialtransparency/IOExercises.scala
@@ -2,15 +2,20 @@ package com.rea.referentialtransparency
 
 import scalaz._, Scalaz._
 import scalaz.effect.IO
+import scala.io.StdIn
 
 object IOExercises {
 
-  def readLineFromConsole(): IO[String] = ???
+  def readLineFromConsole(): IO[String] = IO {
+    StdIn.readLine()
+  }
 
-  def writeToConsole(s: String): IO[Unit] = ???
+  def writeToConsole(s: String): IO[Unit] = IO {
+    println(s)
+  }
 
-  def readFromConsoleAndWriteToConsole: IO[Unit] = ???
+  def readFromConsoleAndWriteToConsole: IO[Unit] = readLineFromConsole flatMap writeToConsole
 
-  def run(effect: IO[Unit]): Unit = ???
+  def run(effect: IO[Unit]): Unit = effect.unsafePerformIO()
 
 }
