59161fd5ba0e042705e676fd0db79bb5bf824c81
Chris Myers <chris.myers@rea-group.com>
2014-05-20 13:44:10 +1000

    Converting main methods to tests.

--- src/main/scala/com/rea/typesafety/Box.scala
+++ src/main/scala/com/rea/typesafety/Box.scala
@@ -6,8 +6,3 @@ case class Box[A](get: A) {
   def flatMap[B](f: A => Box[B]): Box[B] = ???
 }
 
-object BoxExercises extends App {
-  println("Box[6] + 3 = Box[9]: " + Box(6).map(_ + 3))
-  println("Box[4] + 2 = Box[6]: " + Box(4).flatMap(b => Box(b + 2)))
-}
-
