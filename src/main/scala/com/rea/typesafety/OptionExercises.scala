264229bffc87a5aa311ea3a8d269cd475719112a
Chris Myers <chris.myers@rea-group.com>
2014-06-19 16:43:04 +1000

    Implementing Maybe with Answers

--- src/main/scala/com/rea/typesafety/OptionExercises.scala
+++ src/main/scala/com/rea/typesafety/OptionExercises.scala
@@ -33,25 +33,46 @@ object OptionalExercises2 {
 
 object OptionalExercises3 {
 
-  sealed trait Maybe[A]
+  sealed trait Maybe[+A]
 
   case class Just[A](get: A) extends Maybe[A]
 
   case object Nothing extends Maybe[Nothing]
 
-  def flatMap[A, B](m: Maybe[A], f: A => Maybe[B]): Maybe[B] = ???
+  def flatMap[A, B](m: Maybe[A], f: A => Maybe[B]): Maybe[B] = m match {
+    case Just(a) => f(a)
+    case _ => Nothing
+  }
 
-  def map[A, B](m: Maybe[A], f: A => B): Maybe[B] = ???
+  def map[A, B](m: Maybe[A], f: A => B): Maybe[B] = m match {
+    case Just(a) => Just(f(a))
+    case _ => Nothing
+  }
 
-  def fold[A,B](m: Maybe[A], default: => B, f: A => B): B = ???
+  def fold[A, B](m: Maybe[A], default: => B, f: A => B): B = m match {
+    case Just(a) => f(a)
+    case _ => default
+  }
 
-  def orElse[A](m: Maybe[A], otherwise: => Maybe[A]): Maybe[A] = ???
+  def orElse[A](m: Maybe[A], otherwise: => Maybe[A]): Maybe[A] = m match {
+    case Just(a) => m
+    case _ => otherwise
+  }
 
-  def orSome[A](m:Maybe[A], default: => A): A = ???
+  def orSome[A](m: Maybe[A], default: => A): A = m match {
+    case Just(a) => a
+    case _ => default
+  }
 
-  def map2[A,B,C](m1:Maybe[A], m2:Maybe[B], f: (A,B) => C):Maybe[C] = ???
+  def map2[A, B, C](f: (A, B) => C)(m1: Maybe[A], m2: Maybe[B]): Maybe[C] = flatMap(m1, { a: A =>
+    map(m2, { b: B => f(a, b)})
+  })
 
-  def sequence[A](l: List[Maybe[A]]): Maybe[List[A]] = ???
+  def sequence[A](l: List[Maybe[A]]): Maybe[List[A]] = l.foldRight[Maybe[List[A]]](Just(Nil)) {
+    map2((_ :A) :: (_ : List[A]))
+  }
+
+
 
 
 }
