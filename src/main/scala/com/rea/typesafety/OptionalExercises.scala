3d1b3a86258120986278a67b41fe67a1dfc15d4c
Stilianos Menelaou <stilianos.menelaou@rea-group.com>
2017-02-23 11:02:36 +1100

    Fix OptionalExercises
    What was I thinking!?

--- src/main/scala/com/rea/typesafety/OptionalExercises.scala
+++ src/main/scala/com/rea/typesafety/OptionalExercises.scala
@@ -3,25 +3,25 @@ package com.rea.typesafety
 /**
   * Use pattern matching and recursion.  No vars, no loops, no overriding.
   *
-  * A Maybe is our implementation of optional functionality. Scala's is called Option.
+  * `Option` is an implementation of optional functionality.
   *
   * We've made these exercises to give you greater insight into how an optional pattern
   * might work in a functional language.
   *
-  * When you see Maybe think: "Maybe it exists, maybe it doesn't"
+  * When you see `Option` think: "It may exist, or it may not"
   *
-  * There are two ways to construct a Maybe:
+  * There are two ways to construct an `Option`:
   *
-  *   Just() represents something that exists
+  *   `Some()` represents something that exists
   *
-  *   Nothing represents something that doesn't exist
+  *   `None` represents something that doesn't exist
   *
-  * We use Maybe in situations where there isn't certainty that a meaningful
+  * We use `Option` in situations where there isn't certainty that a meaningful
   * value will be returned to us.
   *
   *   The `get()` method on the key to value store `Map` is a great example of this.
   *
-  *     We expect get() to take a key and give us a value in return.
+  *     We expect `get()` to take a key and give us a value in return.
   *
   *     But what happens when our Map doesn't know about the key we gave it?
   *
@@ -36,29 +36,30 @@ package com.rea.typesafety
   *     val myMap = Map[Int, String]( 1 -> "one", 2 -> "two, ...)
   *
   *
-  *   When we call get() on Map we will always get back a Maybe type
+  *   When we call `get()` on Map we will always get back an `Option` type
   *
-  *     myMap.get(1) = Just("one")  //The value exists and it's the string "one"
+  *     myMap.get(1) = Some("one")  //The value exists and it's the string "one"
   *
-  *     myMap.get(0) = Nothing      //The value doesn't exist so we get Nothing
+  *     myMap.get(0) = None      //The value doesn't exist so we get None
   *
-  *   Just("one") and Nothing are both of the type Maybe
+  *   `Some("one")` and `None` are both of the type Option
   *
-  *   Since Just and Nothing are the same type we can pattern match on them!
+  *   Since `Some` and `None` are the same type we can pattern match on them!
   *
-  *   We can have one set of logic when we get Just back and a different set
-  *   of logic when we get Nothing back!
+  *   We can have one set of logic when we get Some back and a different set
+  *   of logic when we get `None` back!
   *
-  *   val mightBeSomething: Maybe[String] = myMap.get(3)
+  *   val mightBeSomething: Option[String] = myMap.get(3)
   *
   *   val result: String = mightBeSomething match {
-  *     case Just(string) => "I got a String back!"
-  *     case Nothing => "I got Nothing back"
+  *     case Some(string) => "I got a String back!"
+  *     case None => "I got None back"
   *   }
   *
   * Good luck!
   *
   */
+
 object OptionalExercises1 {
 
   val config = Map[String, String]("host" -> "rea.com", "port" -> "8080")
@@ -75,14 +76,26 @@ object OptionalExercises2 {
   val hosts = Map("host1" -> "rea.com", "host2" -> "test.rea.com", "host3" -> "netflix.com")
   val envs = Map("rea.com" -> "prod", "test.rea.com" -> "test", "amazon.com" -> "stage")
 
-  def getEnvForHost(host: String): Option[String] = ???
+  // Should return the host string if successful or "couldn't resolve" if unsuccessful
+  def getEnvForHost(host: String): String = ???
 
   // See how many ways you can implement this.
-  // Will either return "Connected to rea.com" or "not connected"
+  // Will either return "Connected to <rea host>" or "not connected"
   def connectToReaHostsOnly(host: String): String = ???
 
   def createConnection(domain: String): String = s"connected to $domain"
 }
+
+/**
+  * Here we make the trait `Maybe`, which is our version of `Option`
+  *
+  * `Just` has the same behavior as `Some`
+  * `Nothing` has the same behavior as `None`
+  *
+  * We use this exercise to illustrate that we can create our own optional behavior
+  * with just a few functions.
+  *
+  */
 
 object OptionalExercises3 {
 
